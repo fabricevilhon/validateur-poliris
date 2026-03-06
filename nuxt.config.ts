6af1e73df42b0d024e3f55f96dc5da94a6f15438
VILHON Fabrice <fvilhon@figarocms.fr>
2026-03-06 15:40:05 +0100

    new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,9 @@ export default defineNuxtConfig({
         { name: 'description', content: 'Outil de validation de fichiers CSV au format Poliris pour les annonces immobilières' }
       ],
       link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap' }
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }
       ]
     }
   },
